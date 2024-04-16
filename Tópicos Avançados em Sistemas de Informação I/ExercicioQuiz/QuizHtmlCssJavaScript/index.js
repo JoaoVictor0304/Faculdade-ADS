835f28c5f8370229fd2b9af65292eb05aaf6609e
João Victor <joaovictor_avelino@hotmail.com>
2024-04-16 11:33:18 -0300

    Exercício html css javascript

---
+++ Tópicos Avançados em Sistemas de Informação I/ExercicioQuiz/QuizHtmlCssJavaScript/index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs');
+const port = 8180;
+
+
+const server=http.createServer((req, res)=>{
+  fs.readFile('./quizz/html.html', (err, arquivo)=>{
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write(arquivo)
+    return res.end()
+  })
+})
+
+server.listen(port, ()=>{
+  console.log('Servidor iniciado com sucesso!');
+})
