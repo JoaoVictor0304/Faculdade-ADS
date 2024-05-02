1ff9f0a23d9af1da299d2deb0cdf9dfe4e31eaa9
João Victor <joaovictor_avelino@hotmail.com>
2024-05-02 00:52:45 -0300

    Exercicio estrutura de dados I
    Exercicio 11-2

---
+++ Estrutura de Dados I/PilhaFilaCompra/src/pckcompras/Compra.java
@@ -0,0 +1,45 @@
+
+package pckcompras;
+
+public class Compra {
+    private String nomeComprador;
+    private String nomeProduto;
+    private float valorProduto;
+
+    public Compra(String nomeComprador, String nomeProduto, float valorProduto) {
+        this.nomeComprador = nomeComprador;
+        this.nomeProduto = nomeProduto;
+        this.valorProduto = valorProduto;
+    }
+
+    public String getNomeComprador() {
+        return nomeComprador;
+    }
+
+    public void setNomeComprador(String nomeComprador) {
+        this.nomeComprador = nomeComprador;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public float getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(float valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" + "nomeComprador=" + nomeComprador + ", nomeProduto=" + nomeProduto + ", valorProduto=" + valorProduto + '}';
+    }
+    
+    
+}
